48cabfc025bad18ba390ff084ec614704cb41626
Alex Wandres <alex.r.wandres@gmail.com>
2025-11-13 10:24:44 -0500

    Fix: Add Prisma Accelerate extension and ensure Prisma Client generation in build

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,15 @@
 import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: ReturnType<typeof createPrismaClient> | undefined;
 }
 
-export const prisma = global.prisma || new PrismaClient();
+function createPrismaClient() {
+  return new PrismaClient().$extends(withAccelerate());
+}
+
+export const prisma = global.prisma || createPrismaClient();
 
 if (process.env.NODE_ENV !== 'production') {
   global.prisma = prisma;
